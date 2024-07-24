5b11cdfacd698c3c5a5eb24837548812366d7bb4
abhayg951 <insuabhay951@gmail.com>
2024-07-24 12:25:59 +0530

    added authentication templates

--- EcomShop/urls.py
+++ EcomShop/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
+    path("auth/", include(("authentication.urls", "auth"), namespace="auth")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
